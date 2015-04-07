d684d028bcad8b7c65062ead21f9681516bdc022
george.s <george.s@u-tx.com>
2015-04-07 12:01:55 +0300

    Added first class and unit tests

---
+++ TestAgent.Core.Tests/FilePackagerTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace TestAgent.Core.Tests
+{
+    [TestFixture]
+    public class FilePackagerTests
+    {
+        [Test]
+        public void CompressFiles_EmptyOutputFileSpecified_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(
+                () => FilePackager.CompressFiles(string.Empty,
+                    Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DummyFile1.txt"),
+                    Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DummyFile2.txt")));
+        }
+
+        [Test]
+        public void CompressFiles_NullOutputFileSpecified_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(
+                () => FilePackager.CompressFiles(null,
+                    Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DummyFile1.txt"),
+                    Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DummyFile2.txt")));
+        }
+
+        [Test]
+        public void CompressFiles_NoFilesToCompressSpecified_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(
+                () => FilePackager.CompressFiles(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "output.zip")));
+        }
+
+        [Test]
+        public void CompressFiles_FilesToCompressDontExist_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(
+                () => FilePackager.CompressFiles(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "output.zip"),
+                    Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DummyFileNonExistent.txt")));
+        }
+
+        [Test]
+        public void CompressFiles_SomeFilesToCompressDontExist_ThrowsArgumentException()
+        {
+            Assert.Throws<ArgumentException>(
+                () => FilePackager.CompressFiles(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "output.zip"),
+                    Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DummyFileNonExistent.txt"),
+                    Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DummyFile2.txt")));
+        }
+
+        [Test]
+        public void CompressFiles_EverythingFine_CreatesOutputFile()
+        {
+            if (File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "output.zip")))
+            {
+                File.Delete(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "output.zip"));
+            }
+
+            FilePackager.CompressFiles(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "output.zip"),
+                    Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DummyFile1.txt"), 
+                    Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DummyFile2.txt"));
+
+            Assert.True(File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "output.zip")));
+        }
+    }
+}
---
+++ TestAgent.Core/FilePackager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace TestAgent.Core
+{
+    public class FilePackager
+    {
+        /// <summary>
+        /// Takes a list of files and creates a zip file with those contents
+        /// </summary>
+        /// <param name="outputFile">The resulting zip filename</param>
+        /// <param name="filenamesToCompress">The files to add to the zip package</param>
+        public static void CompressFiles(string outputFile, params string[] filenamesToCompress)
+        {
+            // Make sure that at least one filename is specified
+            if (filenamesToCompress.Length == 0)
+            {
+                throw new ArgumentException("At least one file to compress should be specified!", "filenamesToCompress");
+            }
+
+            // Make sure that all the files specified exist
+            var filesThatDoNotExist = filenamesToCompress.Where(f => !File.Exists(f)).ToArray();
+            if (filesThatDoNotExist.Length > 0)
+            {
+                string missingFiles = filesThatDoNotExist.Aggregate(string.Empty, (current, file) => current + (file + Environment.NewLine));
+
+                throw new ArgumentException("Some of the files specified do not exist: " + Environment.NewLine + missingFiles, "filenamesToCompress");
+            }
+
+            // Make sure that the output file specified is valid
+            if (string.IsNullOrEmpty(outputFile))
+            {
+                throw new ArgumentException("The output file specified cannot be empty", "outputFile");
+            }
+
+            string directoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "TemporaryForCompression");
+
+            // Delete any existing files in temporary directory
+            if (Directory.Exists(directoryPath))
+            {
+                Directory.Delete(directoryPath, true);
+            }
+
+            // Recreate the directory
+            Directory.CreateDirectory(directoryPath);
+
+            // Copy all files specified to a temporary directory
+            foreach (var file in filenamesToCompress)
+            {
+                string filename = Path.GetFileName(file);
+                File.Copy(file, Path.Combine(directoryPath, filename));
+            }
+
+            // If the output file already exists then delete it (overwrite)
+            if (File.Exists(outputFile))
+            {
+                File.Delete(outputFile);
+            }
+
+            // Compress the directory contents
+            ZipFile.CreateFromDirectory(directoryPath, outputFile);
+        }
+    }
+}
